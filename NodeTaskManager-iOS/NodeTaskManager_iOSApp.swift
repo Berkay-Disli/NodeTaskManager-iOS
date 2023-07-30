3e763e65fa1832e33664ef31305c0df4f8b80579
Berkay Dişli <berkay@abonesepeti.com>
2023-07-30 16:21:21 +0300

    Initial Commit

---
+++ NodeTaskManager-iOS/NodeTaskManager_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NodeTaskManager_iOSApp.swift
+//  NodeTaskManager-iOS
+//
+//  Created by Berkay Dişli on 30.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct NodeTaskManager_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
